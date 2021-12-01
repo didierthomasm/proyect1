6535ee3676f99e349d2cbbd0aef2f68c1a4acc6c
Didier Thomas <didierthomas.m@gmail.com>
2021-12-01 02:06:52 -0600

    Html y Forms

---
+++ 01.Intro/01.First/myScript.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Ya estuvo Ferraz!";
+}
+
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = 5 * 2;
+}
+
+function displayDate() {
+    document.getElementById("demo3").innerHTML = Date();
+}
